aba1e4134531e6382a8e88b6b0e4ed488563b2c2
nitori <nitori@kagari.fun>
2021-12-20 19:36:37 +0800

    move public files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, viewport-fit=cover" />
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
